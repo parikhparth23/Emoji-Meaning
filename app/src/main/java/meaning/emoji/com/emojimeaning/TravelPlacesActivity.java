1962e02a82a025a87865016f1b155993324e61ff
parikhparth23 <parikhparth23@gmail.com>
2019-10-10 14:03:23 -0700

    added support for android 10 and added app icon

--- app/src/main/java/meaning/emoji/com/emojimeaning/TravelPlacesActivity.java
+++ app/src/main/java/meaning/emoji/com/emojimeaning/TravelPlacesActivity.java
@@ -1,7 +1,7 @@
 package meaning.emoji.com.emojimeaning;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ListView;
 
 import java.util.ArrayList;
